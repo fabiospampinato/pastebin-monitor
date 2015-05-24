9bbc46ace5871f1c5b689cd2aef1e3fbf74bad49
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-05-24 04:59:37 +0100

    Still fixing the encoding issue

--- pastebin_crawler.py
+++ pastebin_crawler.py
@@ -7,6 +7,101 @@ import sys
 
 from pyquery import PyQuery
 
+def all_python_encodings():
+     return ["ascii",
+             "big5",
+             "big5hkscs",
+             "cp037",
+             "cp424",
+             "cp437",
+             "cp500",
+             "cp720",
+             "cp737",
+             "cp775",
+             "cp850",
+             "cp852",
+             "cp855",
+             "cp856",
+             "cp857",
+             "cp858",
+             "cp860",
+             "cp861",
+             "cp862",
+             "cp863",
+             "cp864",
+             "cp865",
+             "cp866",
+             "cp869",
+             "cp874",
+             "cp875",
+             "cp932",
+             "cp949",
+             "cp950",
+             "cp1006",
+             "cp1026",
+             "cp1140",
+             "cp1250",
+             "cp1251",
+             "cp1252",
+             "cp1253",
+             "cp1254",
+             "cp1255",
+             "cp1256",
+             "cp1257",
+             "cp1258",
+             "euc_jp",
+             "euc_jis_2004",
+             "euc_jisx0213",
+             "euc_kr",
+             "gb2312",
+             "gbk",
+             "gb18030",
+             "hz",
+             "iso2022_jp",
+             "iso2022_jp_1",
+             "iso2022_jp_2",
+             "iso2022_jp_2004",
+             "iso2022_jp_3",
+             "iso2022_jp_ext",
+             "iso2022_kr",
+             "latin_1",
+             "iso8859_2",
+             "iso8859_3",
+             "iso8859_4",
+             "iso8859_5",
+             "iso8859_6",
+             "iso8859_7",
+             "iso8859_8",
+             "iso8859_9",
+             "iso8859_10",
+             "iso8859_13",
+             "iso8859_14",
+             "iso8859_15",
+             "iso8859_16",
+             "johab",
+             "koi8_r",
+             "koi8_u",
+             "mac_cyrillic",
+             "mac_greek",
+             "mac_iceland",
+             "mac_latin2",
+             "mac_roman",
+             "mac_turkish",
+             "ptcp154",
+             "shift_jis",
+             "shift_jis_2004",
+             "shift_jisx0213",
+             "utf_32",
+             "utf_32_be",
+             "utf_32_le",
+             "utf_16",
+             "utf_16_be",
+             "utf_16_le",
+             "utf_7",
+             "utf_8",
+             "utf_8_sig"]
+
+
 class Logger:
 
     shell_mod = {
@@ -53,7 +148,7 @@ class Crawler:
     OK = 1
     ACCESS_DENIED = -1
     CONNECTION_FAIL = -2
-    OTHER_ERROR = -2
+    OTHER_ERROR = -3
 
     prev_checked_ids = []
     new_checked_ids = []
@@ -88,11 +183,15 @@ class Crawler:
         try:
             page_html = page.html ()
         except:
-            try:
-                page_html = page.html(encoding='ascii')
-            except:
+            worked = False
+            for enc in all_python_encodings():
+                try:
+                    page_html = page.html(encoding=enc)
+                    worked = True
+                except:
+                    pass
+            if not worked:
                 return self.OTHER_ERROR, None
-
         if re.match ( r'Pastebin\.com - Access Denied Warning', page_html, re.IGNORECASE ) or 'blocked your IP' in page_html:
             return self.ACCESS_DENIED,None
         else:
@@ -162,7 +261,7 @@ class Crawler:
                     Logger ().log ( 'Please wait ' + str ( ban_wait - n ) + ' minute' + ( 's' if ( ban_wait - n ) > 1 else '' ) )
                     time.sleep ( 60 )
             elif status == self.CONNECTION_FAIL:
-                Logger().log ( 'Connection down. Waiting {:s} seconds and trying again'.format(connection_timeout), True, 'RED')
+                Logger().log ( 'Connection down. Waiting {:d} seconds and trying again'.format(connection_timeout), True, 'RED')
                 time.sleep(connection_timeout)
             elif status == self.OTHER_ERROR:
                 Logger().log('Unknown error. Maybe an encoding problem? Trying again.'.format(connection_timeout), True,'RED')
